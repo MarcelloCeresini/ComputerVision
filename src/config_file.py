e2ab540aa1aa2942ee8be14f2a9092fa32688252
MarcelloCeresini <mceresini.97@gmail.com>
2023-03-03 17:52:04 +0100

    started input + evaluation

--- src/config_file.py
+++ src/config_file.py
@@ -1,4 +1,10 @@
 class Config:
 
     def __init__(self, *args, **kwargs):
-        pass
+        self.disp_min = 0
+        self.disp_max = 15
+        self.disp_scale = 16
+        self.ignore_border = 18
+        self.d_range = range(self.disp_min, self.disp_max+1)
+
+        
